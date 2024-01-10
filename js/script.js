3efedf89c457c5e6b00b89811f70d6c14d5cb418
Mich <michxcrz@gmail.com>
2024-01-10 15:23:54 -0300

    imagem cachorrinhos

--- js/script.js
+++ js/script.js
@@ -1,9 +1,15 @@
 const keys = document.querySelectorAll('.key');
-
+const playNote = (note) => {
+    const audio = new Audio('../notes/1.wav');
+    audio.play();
+}
 
 const handleMouseDown = (key) =>{
 
+    playNote()
+
     if(key.className.includes('black')){
+        key.classList.add('black--pressed')
         return;
     }
 
@@ -11,6 +17,12 @@ const handleMouseDown = (key) =>{
 }
 
 const handleMouseUp = (key) =>{
+
+    if(key.className.includes('black')){
+        key.classList.remove('black--pressed')
+        return;
+    }
+
     key.style.background = 'white';
 }
 
