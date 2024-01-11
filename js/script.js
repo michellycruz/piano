6c037c346c8679f1da376a7d133af1df44115f68
Mich <michxcrz@gmail.com>
2024-01-11 14:38:36 -0300

    notas

--- js/script.js
+++ js/script.js
@@ -50,10 +50,34 @@ checkbox.addEventListener('change', ({ target }) => {
     keysSection.classList.add('disabled-keys');
 })
 
-const keydownMapper = {
-    'q': 'Você digitou'
+const keyDownMapper = {
+    'Tab': () => handleMouseDown(keys[0]),
+    '1': () => handleMouseDown(keys[1]),
+    'q': () => handleMouseDown(keys[2]),
+    '2': () => handleMouseDown(keys[3]),
+    'w': () => handleMouseDown(keys[4]),
+    'e': () => handleMouseDown(keys[5]),
+    '4': () => handleMouseDown(keys[6]),
+    'r': () => handleMouseDown(keys[7]),
+    '5': () => handleMouseDown(keys[8]),
+    't': () => handleMouseDown(keys[9]),
+    '6': () => handleMouseDown(keys[10]),
+    'y': () => handleMouseDown(keys[11]),
+    'u': () => handleMouseDown(keys[12]),
+    '8': () => handleMouseDown(keys[13]),
+    'i': () => handleMouseDown(keys[14]),
+    '9': () => handleMouseDown(keys[15]),
+    'o': () => handleMouseDown(keys[16]),
+    'p': () => handleMouseDown(keys[17]),
+    '-': () => handleMouseDown(keys[18]),
+    '0': () => handleMouseDown(keys[19]),
+    '=': () => handleMouseDown(keys[20]),
+    '[': () => handleMouseDown(keys[21]),
+    'Backspace': () => handleMouseDown(keys[22]),
+    ']': () => handleMouseDown(keys[23])
 }
 
 document.addEventListener('keydown', (event) => {
-    alert(keydownMapper[event.key])
+   keyDownMapper[event.key]()
+   console.log(event.key)
 })
